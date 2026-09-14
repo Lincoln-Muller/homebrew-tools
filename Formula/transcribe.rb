70368c8e47350eb45f8a43a9104ece067271924c
Thyssenkrupp234 <thyssenkrupp234@gmail.com>
2026-09-13 17:33:34 -0700

    Polish transcribe formula

--- Formula/transcribe.rb
+++ Formula/transcribe.rb
@@ -2,8 +2,8 @@ class Transcribe < Formula
   desc "Batch audio and video transcription on Apple silicon"
   homepage "https://github.com/Lincoln-Muller/homebrew-tools"
   url "https://raw.githubusercontent.com/Lincoln-Muller/homebrew-tools/main/transcribe/transcribe", using: :nounzip
-  sha256 "06dbf1d0c4be2ac5f163507d41cfe4e03285611a4eadf899b4e1c121cefb542d"
   version "0.1.0"
+  sha256 "06dbf1d0c4be2ac5f163507d41cfe4e03285611a4eadf899b4e1c121cefb542d"
 
   depends_on arch: :arm64
   depends_on "ffmpeg"
